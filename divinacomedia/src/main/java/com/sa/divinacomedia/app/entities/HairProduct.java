7cb97a61092f50743c71dfb49edc9ade2f8e5816
IvonneTVallejo <84489188+IvonneTVallejo@users.noreply.github.com>
2021-12-09 09:51:18 -0500

    Cambios Reto2

--- divinacomedia/src/main/java/com/sa/divinacomedia/app/entities/HairProduct.java
+++ divinacomedia/src/main/java/com/sa/divinacomedia/app/entities/HairProduct.java
@@ -21,12 +21,20 @@ public class HairProduct {
     @Id
     @JsonProperty("reference")
     private String id;
+    @JsonProperty("brand")
     private String brand;
+    @JsonProperty("category")
     private String category;
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("availability")
     private boolean availability = true;
+    @JsonProperty("price")
     private double price;
+    @JsonProperty("quantity")
     private int quantity;
+    @JsonProperty("photography")
     private String photography;
 }
