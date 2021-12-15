d69d11fcb63f4609fa3c46750a9aff600268ba62
IvonneTVallejo <84489188+IvonneTVallejo@users.noreply.github.com>
2021-12-14 22:07:08 -0500

    Cambios Reto2 cambios 1007

--- divinacomedia/src/main/java/com/sa/divinacomedia/app/repositories/OrderRepository.java
+++ divinacomedia/src/main/java/com/sa/divinacomedia/app/repositories/OrderRepository.java
@@ -4,10 +4,14 @@ import com.sa.divinacomedia.app.entities.Order;
 import com.sa.divinacomedia.app.repositories.crud.OrderCrudRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -54,12 +58,16 @@ public class OrderRepository {
 
     public List<Order> getByRegisterDayAndSalesManId(String registerDay, Integer id){
 
-        try {
-            return repository.findByRegisterDayAndSalesManId(new SimpleDateFormat("yyyy-MM-dd").parse(registerDay),id);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Query query = new Query();
 
+        Criteria dataCriteria = Criteria.where("registerDay")
+                .gte(LocalDate.parse(registerDay, dtf).minusDays(1).atStartOfDay())
+                .lt(LocalDate.parse(registerDay,dtf).plusDays(1).atStartOfDay())
+                .and("salesMan.id").is(id);
+
+        query.addCriteria(dataCriteria);
+        List<Order> orders = mongoTemplate.find(query,Order.class);
+        return orders;
     }
 }
